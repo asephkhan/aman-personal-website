e193193b0dfdf3c518e112805f89414b7beb47e8
Asif Ali <asephkhan@gmail.com>
2022-06-24 04:58:31 +0100

    REDUCED THE FADING SPEED

--- burger.js
+++ burger.js
@@ -13,7 +13,7 @@ const navSlide = () => {
         if (link.style.animation) {
           link.style.animation = "";
         } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${
+          link.style.animation = `navLinkFade 0.3s ease forwards ${
             index / 7 + 1
           }s`;
         }
